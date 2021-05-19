be26c2c57f5587b207d709c229db8200ec28ba1a
Victor Umunna <umunnavictor01@gmail.com>
2021-05-19 22:54:17 +0100

    update app

--- myapp.py
+++ myapp.py
@@ -17,5 +17,12 @@ tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
 tickerDf = tickerData.history(period = '1d', start = '2010-5-31', end = '2020-5-31')
 
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume
+""")
 st.line_chart(tickerDf.Volume)
